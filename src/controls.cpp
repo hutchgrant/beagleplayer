753625a2e17f31434180f84c53aff0d2b1ba1308
Grant Hutchinson <h.g.utchinson@gmail.com>
2012-05-14 05:15:39 -0400

    adding playlist widget features include: add, save, remove.

--- src/controls.cpp
+++ src/controls.cpp
@@ -70,6 +70,28 @@ void controls::startSelected(){
     startLocal(finSong, finPath);
 }
 /*
+  *  Sort the current play list
+  */
+void controls::startPlaylistSelected(fileObj &PLCurrent){
+    int finSongSize = 0;
+    int finPathSize = 0;
+    int selID = 0;
+    char *finPath;
+    char *finSong;
+
+    selID = curList[CurrentSelect];
+    cout << "selection id " << selID << endl;
+    finSongSize = strlen(checkSongObjByID(selID, PLCurrent));
+    finSong = new char[finSongSize + 100];
+    finSong = checkSongObjByID(selID, PLCurrent);
+
+    finPathSize = strlen(checkSongObjPathByID(selID, PLCurrent));
+    finPath = new char[finPathSize + 100];
+    finPath = checkSongObjPathByID(selID, PLCurrent);
+
+    startLocal(finSong, finPath);
+}
+/*
   * Slot to Change connection Mode
   */
 void controls::changeCon(int mode){
@@ -82,6 +104,17 @@ void controls::changeCon(int mode){
 void controls::setCurList(fileObj &newList, int *newIDlist){
     current = newList;
     curList = newIDlist;
+}
+
+/*
+  * Set Current playlist list when list changes
+  */
+void controls::setCurPLAYList(fileObj &newList, int *newIDlist, int selected){
+    curList = newIDlist;
+    current.initFile(100);
+    current = newList;
+    CurrentSelect = selected;
+    startPlaylistSelected(newList);
 }
 
 /*
